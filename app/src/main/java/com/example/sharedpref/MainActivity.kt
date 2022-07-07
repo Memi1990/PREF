79edd5c013d4739645600a94be4a9112677a14cc
Kotlin <noemilopezpina@gmail.com>
2022-07-07 18:01:06 +0200

    commit inicial

---
+++ app/src/main/java/com/example/sharedpref/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.sharedpref
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.sharedpref.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var b:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+        initUi()
+        checkUserValues()
+    }
+    fun initUi(){
+        b.btnAccess.setOnClickListener {
+            accessSharedPreferences()
+        }
+    }
+    fun checkUserValues(){
+        if (prefs.getName().isNotEmpty()){
+            goAccess()
+        }
+    }
+    fun accessSharedPreferences(){
+        if (b.edName.text.toString().isNotEmpty()){
+            prefs.saveName(b.edName.text.toString())
+            prefs.saveColor(b.checkBox.isChecked)
+            goAccess()
+            } else {
+                Toast.makeText(this, "Debe rellenar el nombre", Toast.LENGTH_SHORT).show()
+            }
+    }
+    private fun goAccess(){
+        startActivity(Intent(this,AccessActivity::class.java))
+    }
+}
