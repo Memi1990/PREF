f2b2c7c3116cd1abef006c3c51922cd69512d9cb
Kotlin <noemilopezpina@gmail.com>
2022-07-08 16:31:31 +0200

    commit access finished

--- app/src/main/java/com/example/sharedpref/AccessActivity.kt
+++ app/src/main/java/com/example/sharedpref/AccessActivity.kt
@@ -2,6 +2,8 @@ package com.example.sharedpref
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.sharedpref.SharedPreferences.Companion.prefs
 import com.example.sharedpref.databinding.ActivityAccessBinding
 
 class AccessActivity : AppCompatActivity() {
@@ -10,6 +12,17 @@ class AccessActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         b = ActivityAccessBinding.inflate(layoutInflater)
         setContentView(b.root)
-//        initUI()
+        initUI()
+    }
+    fun initUI(){
+        b.btnClose.setOnClickListener {
+            prefs.wipeData()
+            onBackPressed()
+        }
+        val userName = prefs.getName()
+        b.tvHola.text = "¡Hola $userName!"
+        if (prefs.getColorCheck()){
+            b.containerLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.grey_blue))
+        }
     }
 }
